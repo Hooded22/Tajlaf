c863687171d103d29cc48b2b1de410c664046f0e
Przemek Sipta <przemek.sipta@gmail.com>
2020-03-08 17:27:14 +0100

    Info Box repearing
    *Info box render item system
    *Getting info from json file
    *Hide and show info box

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
 
-class CatSwitcher{
+class ImageBoxController{
     constructor()
     {
         this.posterDiv = document.getElementById("introSection_poster");
@@ -22,34 +22,75 @@ class CatSwitcher{
 
     changeClassOfDivs = (name) => {
         const ButtonsDiv = this.posterDiv.getElementsByClassName("Buttons")[0].children;
-        const contentUl = this.posterDiv.getElementsByTagName("ul");
+        const InfoBox = new InfoBoxController();
         
         if(name == "Cat")
         {
             ButtonsDiv[0].setAttribute("class","active");
             ButtonsDiv[1].setAttribute("class","");
-            
-            contentUl[0].setAttribute("class","active");
-            contentUl[1].setAttribute("class","");
+            this.currentImageIsCat = true
+            InfoBox.generateContent(true);
         }
         else
         {
             ButtonsDiv[0].setAttribute("class","");
             ButtonsDiv[1].setAttribute("class","active");
-
-            contentUl[0].setAttribute("class","");
-            contentUl[1].setAttribute("class","active");
+            this.currentImageIsCat = false;
+            InfoBox.generateContent(false);
         }
-
     }
 }
 
-const cs = new CatSwitcher();
-const posterBox_buttons = document
-.getElementById("introSection_poster")
-.getElementsByClassName("Buttons")[0].children
-posterBox_buttons[0].addEventListener("click",() => {cs.setCatName(posterBox_buttons[0])}, false);
-posterBox_buttons[1].addEventListener("click",() => {cs.setCatName(posterBox_buttons[1])}, false);
+class InfoBoxController{
+    constructor()
+    {
+        this.posterDiv = document.getElementById("introSection_poster");
+        this.generateContent = this.generateContent.bind(this);
+        this.hideAndShow = this.hideAndShow.bind(this);
+        this.infoBoxHidden = false;
+    }
+
+    hideAndShow = () => {
+        const infoBox = this.posterDiv.getElementsByClassName('contentBox')[0].getElementsByTagName('ul');
+        const newClass = this.infoBoxHidden ? "active" : "hide";
+        this.infoBoxHidden = !this.infoBoxHidden;
+        Object.keys(infoBox).map(key => {
+            infoBox[key].setAttribute("class",newClass);
+        })
+    }
+
+    generateContent = (isCat) => {
+        const content = this.posterDiv.getElementsByClassName('contentBox')[0].getElementsByTagName('ul');
+        var catInfo;
+        $.getJSON("../JSON/catInfo.json", function(json) {
+            catInfo = json[isCat ? "Cat" : "Werecat"];
+            content[0].innerHTML = "";
+        }).then(() => {
+            catInfo.map(item => {
+                var node = document.createElement("LI");
+                var textnode = document.createTextNode(item);
+                node.appendChild(textnode); 
+                content[0].appendChild(node);
+            })
+        });
+    }
+
+}
+
+
+window.onload = () => {
+    const Image_Box_Controller = new ImageBoxController();
+    const Info_Box_Controller = new InfoBoxController();
+    const posterBox_buttons = document
+    .getElementById("introSection_poster")
+    .getElementsByClassName("Buttons")[0].children
+    posterBox_buttons[0].addEventListener("click",() => {Image_Box_Controller.setCatName(posterBox_buttons[0])}, false);
+    posterBox_buttons[1].addEventListener("click",() => {Image_Box_Controller.setCatName(posterBox_buttons[1])}, false);
+
+    document.getElementById('contentBox_arrow').addEventListener("click",() => {Info_Box_Controller.hideAndShow()},false);
+    Info_Box_Controller.generateContent(true);
+    
+}
 
 
 
